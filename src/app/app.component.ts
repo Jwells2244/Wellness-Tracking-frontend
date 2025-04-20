a9d600463d140bc2ca08c36b0cdce77232d349ef
aidenballou <155033316+aidenballou@users.noreply.github.com>
2025-04-20 17:40:50 -0400

    not working DO NOT MERGE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  onVideosClick() {
-    console.log('Videos link clicked!');
-  }
-  title = 'your-angular-project';
+  title = 'WellnessTrackingFrontend';
 }
--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,28 @@ import { appConfig } from './app/app.config';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './app/auth.intercepter'; // adjust the path if needed
 
+// Angular Material & Animations
+import { BrowserAnimationsModule }    from '@angular/platform-browser/animations';
+import { MatToolbarModule }           from '@angular/material/toolbar';
+import { MatButtonModule }            from '@angular/material/button';
+import { MatCardModule }              from '@angular/material/card';
+import { MatFormFieldModule }         from '@angular/material/form-field';
+import { MatInputModule }             from '@angular/material/input';
+
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
     ...appConfig.providers,
     importProvidersFrom(RouterModule.forRoot(routes)),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    provideRouter(routes)
+    provideRouter(routes),
+
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      MatToolbarModule,
+      MatButtonModule,
+      MatCardModule,
+      MatFormFieldModule,
+      MatInputModule)
   ]
 }).catch(err => console.error(err));
